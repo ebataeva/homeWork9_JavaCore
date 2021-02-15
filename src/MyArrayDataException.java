734af001fbed541f9de27748a1d3d14b4f7e45d9
Батаева Елена Валентиновна <EBataeva@stream.ru>
2021-02-15 22:16:35 +0300

    убрано ненужное наследование Exception

--- src/MyArrayDataException.java
+++ src/MyArrayDataException.java
@@ -1,7 +1,7 @@
 public class MyArrayDataException extends Exception {
     public MyArrayDataException(String n, int i, int j) {
 
-        super("некорректные данные в ячейке"+n+" "+i+" "+j);
+        super("некорректные данные "+n+" в ячейке "+i+" "+j);
 
     }
 
